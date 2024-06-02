48acbf8990bb8d4d9bb0be635d30ab676077e88e
mQueue <tomorrow9913@gmail.com>
2024-06-02 17:06:40 +0900

    Refactor: 필요 없는 부분 삭제

--- test/issue_test.py
+++ test/issue_test.py
@@ -2,7 +2,7 @@ from starlette.config import Config
 
 from utils.git.github_utils import create_github_issue, get_github_repo
 
-config = Config('.env')
+config = Config('../.env')
 repo = get_github_repo(config('ACCESS_TOKEN'), config('ORG_NAME'), config('REPO_NAME'))
 print(repo)
 
